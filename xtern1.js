18c06231987d5b6986ba86fae8a8bddb0dcb67ed
Lex Anderson <lastyle11@lexs-mbp-2.coldspring.marian.edu>
2017-06-06 09:47:51 -0400

    Append the name as a proper DOM element

--- xtern1.js
+++ xtern1.js
@@ -5,7 +5,10 @@ function handleSubmit(ev) {
     const details = document.querySelector('#details')
     const name = f.personName.value
 
-    details.innerHTML = `<em>${name}</em>`
-    
+   // details.innerHTML = `<em>${name}</em>`
+
+   const em = document.createElement('em')
+   em.textContent = name
+   details.appendChild(em)
 }
 personForm.addEventListener('submit', handleSubmit)
